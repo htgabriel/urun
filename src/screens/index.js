13924228817b0e0d732814681cf8b5f81bd32cc1
Gabriel Henrique <htm.gabriel@gmail.com>
2022-09-26 02:55:48 -0300

    adicionadas as telas: Menu, Message, PasswordRecovery, Profile e Register

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,7 +2,12 @@ import Activities from "./Activities/Activities";
 import Challenges from "./Challenges";
 import Home from "./Home";
 import Login from "./Login";
+import Menu from "./Menu";
 import MenuActivities from "./Activities/MenuActivities";
+import Message from "./Messages";
+import PasswordRecovery from "./PasswordRecovery";
+import Profile from "./Profile";
+import Register from "./Register";
 import Training from "./Training";
 import VirtualRace from "./VirtualRace";
 
@@ -11,7 +16,12 @@ export {
 	Challenges,
 	Home,
 	Login,
+	Menu,
 	MenuActivities,
+	Message,
+	PasswordRecovery,
+	Profile,
+	Register,
 	Training,
 	VirtualRace
 };
